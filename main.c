a329a6d45ecb111faa01ab9074c93624ff3a26c2
jordanlee02 <jordan.lee02@outlook.com>
2024-04-09 21:51:04 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+// Header file for input output functions
+#include <stdio.h>
+#include <stdlib.h>
+
+// main function -
+// where the execution of program begins
+int main()
+{
+
+    // prints welcome message
+    printf("Hello World\n");
+    return 0;
+}
