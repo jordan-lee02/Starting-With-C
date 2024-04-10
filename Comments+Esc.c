8daf4553b3ef1b6c2219a51669d69f57764639da
jordanlee02 <jordan.lee02@outlook.com>
2024-04-10 23:24:16 +0100

    finished the example of comments and escape sequences

--- Comments+Esc.c
+++ Comments+Esc.c
@@ -6,12 +6,17 @@ int main(){
     // This is how you create a comment
 
     /*this is how you would do a multi line comment
+        \n - newline
+        \t = tab
     
     
     */
-    printf("Hello World\n");
-    printf("The quick brown fox jumps over the lazy dog.\n");
+    //use \" \" to add quotations into outputted text
+    printf("\"Hello World\" - The Generic starting point in coding ;)\n");
+
+    //Creates a grid of numbers by using the new line and tab function
+    printf("\n1\t2\t3\n4\t5\t6\n7\t8\t9\n");
 
 
     return 0;
-}
+} 
