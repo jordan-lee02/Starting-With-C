e17f6db836b27a2a3b9bf1684bf661dd038b5f2b
jordanlee02 <jordan.lee02@outlook.com>
2024-04-10 22:57:54 +0100

    more .exe files added to gtignore and a comments and escape example file was created

---
+++ Comments+Esc.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main(){
+
+    // This is how you create a comment
+
+    /*this is how you would do a multi line comment
+    
+    
+    */
+    printf("Hello World\n");
+    printf("The quick brown fox jumps over the lazy dog.\n");
+
+
+    return 0;
+}
