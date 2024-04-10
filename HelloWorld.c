0e60b1ad87e8ff46d0cda37b909da974145be46c
jordanlee02 <jordan.lee02@outlook.com>
2024-04-10 22:51:50 +0100

    Added .exe files to git ignore and added another output line

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,13 +1,15 @@
-// Header file for input output functions
+// #include is a pre processor command that tells the compiler to include the contents of a file
+// <stdio.h> is a file that contains some standard input/output functions that can now be used once included
 #include <stdio.h>
-#include <stdlib.h>
 
 // main function -
 // where the execution of program begins
-int main()
-{
+int main(){
 
-    // prints welcome message
+    // prints welcome message with \n at the end that creates a new line
     printf("Hello World\n");
+    printf("The quick brown fox jumps over the lazy dog.\n");
+
+    //the below function will return 0 if no errors but if errors will return a 1
     return 0;
 }
