2a836f56c57dd320836c35d5a87ac0f48c9fff80
jordanlee02 <jordan.lee02@outlook.com>
2024-04-11 22:00:43 +0100

    Completed example of constants and added .exe to gitignore

--- constants.c
+++ constants.c
@@ -2,11 +2,12 @@
 
 int main(){
 
-   
+    //constant = fixed value that cannot be changed by the program during its execution
+
+    const float PI = 3.141592; - //common naming practice for any constants is to fully capitalise the variables
 
 
-
-    
+    printf("%f", PI);
 
 
     return 0;
