bfc8f3bbd4ff68c47fc4986f5f9b071b57af880b
jordanlee02 <jordan.lee02@outlook.com>
2024-04-11 17:52:52 +0100

    Added examples of variables

--- variables.c
+++ variables.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 
 int main(){
+    
+
+    //variable = Allocated space in memory to store a value.
+    
+    //declaring a variable
+    int x;
+
+    //initialization of a variable
+    x = 123;
+    //combination of initialisation and declaration
+    int y = 321;
+
+    int age = 21; //integer
+    float gpa = 2.05; //floating point(decimal) number
+    char grade = 'C'; //single character
+    char name[] = "Bro"; //array of characters
+
+    printf("");
 
 
-    printf("\n\"Hello World\"\n");
+
+    
+    
+
+
 
     return 0;
 } 
