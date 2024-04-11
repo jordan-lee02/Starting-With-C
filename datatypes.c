7e47d0891255d28d50e5ab7f40ecc20537e138cd
jordanlee02 <jordan.lee02@outlook.com>
2024-04-11 20:24:51 +0100

    Added datatypes.exe to gitignore

--- datatypes.c
+++ datatypes.c
@@ -2,10 +2,16 @@
 #include <stdbool.h>
 
 int main(){
+
+    char a = 'C';           //single character   %c
+    char b[] = "Bro";       //array of characters %s
+
+    float c = 3.141592;         // 4 bytes (32 bits of precision) 6 - 7 digits %f
+    //double d 
     
 
     
 
-    
+
     return 0;
 } 
