e89d58b8aac855f06f3372682279b65c8354b292
jordanlee02 <jordan.lee02@outlook.com>
2024-04-12 00:38:31 +0100

    Started creating an example program to read user input, store them as variables and output the result

--- userinput.c
+++ userinput.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main(){
 
-    
+    char name[25]; //25 byte limit
+    int age;
+
+    //asks the user for their name and stores the variable as a character array called name
+    printf("\nWhat's your name?");
+    //scanf("%s", &name);       //the scanf function will read up to any 'white space'
+    fgets(name, 25, stdin);     //
+    name[strlen(name)-1] = '\0';
+
+
+    //ask the user for an input and store it as an integer variable named age
+    printf("How old are you?");
+    scanf("%d", &age);
+
+    //output a statement including the character array name
+    printf("\nHello %s, how are you?", name);
+
+    //output a statement including the stored variable called age 
+    printf("\nYou are %d years old.", age);
+
+    return 0;
+
 
 }
