ec212bb03c2f493c72d3b045aa8c82a4234b6a3d
jordanlee02 <jordan.lee02@outlook.com>
2024-04-11 20:16:10 +0100

    Finished variables and created datatype example file

---
+++ datatypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    
+
+    
+
+    
+    return 0;
+} 
--- variables.c
+++ variables.c
@@ -18,14 +18,10 @@ int main(){
     char grade = 'C'; //single character
     char name[] = "Bro"; //array of characters
 
-    printf("");
-
-
-
-    
-    
-
-
+    printf("Hello %s\n",name);
+    printf("You are %d years old\n",age);
+    printf("Your average grade is %c\n",grade);
+    printf("Your gpa is %.2f\n",gpa);
 
     return 0;
 } 
