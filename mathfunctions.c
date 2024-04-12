8c9494f68f2cc30a9dd9add0f1142542a3fabd1d
jordanlee02 <jordan.lee02@outlook.com>
2024-04-12 21:20:09 +0100

    Created math function program and removed .exe to gitignore

---
+++ mathfunctions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double A = sqrt(9); //sqaure root function
+    double B = pow(2, 4); //power function
+    int C = round(3.14); //round function
+    int D = ceil(3.14); //round up function
+    int E = floor(3.99); //round down function
+    double F = fabs(-100); //aboslute value function e.g. makes negative numbers positive
+    double G = log(3); //logarthim function
+    double H = sin(45);
+    double I = cos(45);
+    double J = tan(45);
+
+    printf("\n%lf", J);
+
+    return 0;
+}
