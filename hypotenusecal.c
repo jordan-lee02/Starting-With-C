1272f8683d2d1fb0e38b0833bb094695a5955e04
jordanlee02 <jordan.lee02@outlook.com>
2024-04-12 21:50:17 +0100

    Completed simple Hypotenuse Calculator program and added .exe to gitignore

--- hypotenusecal.c
+++ hypotenusecal.c
@@ -3,8 +3,27 @@
 
 int main(){
     
-    
+    double a;
+    double b;
+    double c;
 
+    //welcome message
+    printf("\nHello and Welcome to my Hypotenuse Calculator Program!\n");
+    
+    //asks user for input of side A
+    printf("\nPlease enter side A: ");
+    scanf("%lf", &a);
+
+    //asks user for input of side B
+    printf("Please enter side B: ");
+    scanf("%lf", &b);
+
+    //calculates the formula of the Hypotenuse and stores that value as c
+    c = sqrt(a*a + b*b);
+
+
+    //outputs the result
+    printf("The Hypotenuse(Side C) is: %lf", c);
 
     return 0;
 }
