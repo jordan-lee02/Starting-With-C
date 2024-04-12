c2d16461060b6923753c20d95068577e219b1cb5
jordanlee02 <jordan.lee02@outlook.com>
2024-04-12 21:40:52 +0100

    Added some more comments to help explain the circumference program and created a hypotenuse calculator c file

--- circlecircumferenceprogram.c
+++ circlecircumferenceprogram.c
@@ -8,16 +8,21 @@ int main(){
     double diameter;
     double area;
 
-
-    printf("\nPlease enter a radius of a circle:");
+    //ask user for a radius input and store under variable radius
+    printf("\nPlease enter a radius of a circle: ");
     scanf("%lf", &radius);
 
+    //assign the value of raidus *2 to diameter
     diameter = radius * 2;
     //printf("\nDiameter Check:%lf",diameter);
 
+    //assign the value result of the cirumference to value circumference
     circumference = PI * diameter;
+    
+    //assign result of area calculation to value area
     area = PI * radius * radius;
 
+    //output results of both calculations to user
     printf("\nThe Circumference of your circle is: %lf", circumference);
     printf("\nThe Area of your circle is: %lf", area);
 
