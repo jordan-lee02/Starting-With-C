f8f14b77dc3a00cb687dc02359b70f109c4025ca
jordanlee02 <jordan.lee02@outlook.com>
2024-04-15 23:03:36 +0100

    Created basic example of for loop and moved .exe to gitignore

---
+++ forloops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    //for loop - repeats a section of code a limited amount of times
+
+    for(int i = 10; i >= 1; i-- ){
+
+        printf("\n%d", i);
+
+    }
+
+    return 0;
+}
