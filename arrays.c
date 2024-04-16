cf9782dff0b3d9ed4eb27f04cb466524c748d78c
jordanlee02 <jordan.lee02@outlook.com>
2024-04-16 23:14:25 +0100

    Created basic array example and moved respective .exe to gitignore

---
+++ arrays.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    // array = a data structure that can store many values of the same data type.
+    double prices[10] = {5.0, 10.0, 15.0, 25.0, 20.0};// length of an array is number of values - 1 as the first value is classed as 0
+
+    //prices[0] = 5.0;
+    //prices[1] = 10.0;
+    //prices[2] = 15.0;
+    //prices[3] = 25.0;
+    //prices[4] = 20.0;
+
+    printf("$%.2lf", prices[4]); 
+
+
+     
+
+
+
+    return 0;
+}
