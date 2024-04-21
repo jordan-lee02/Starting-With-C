4131ecc2b52b074a6bd64aaa6e617536e5589df2
jordanlee02 <jordan.lee02@outlook.com>
2024-04-21 22:35:00 +0100

    Added examples of memory addresses and created a pointers example file.

---
+++ src/pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    // pointer = a "variable-like" reference that holds a memory address to another variable, arry etc.
+            //  some tasks are performed more easily with pointers
+    //          * = indirection operator (value at address)
+
+    
+
+
+
+    return 0;
+}
