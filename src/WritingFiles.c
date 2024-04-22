9cda362e3b3785cab1e2199275934afb3f3b818c
jordanlee02 <jordan.lee02@outlook.com>
2024-04-23 00:49:40 +0100

    Created writing files example

---
+++ src/WritingFiles.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+
+    /*
+    FILE *pF = fopen("testwrite.txt", "a");
+
+    fprintf(pF, "\nSpongebob Sqaurepants");
+
+    fclose(pF);
+    */
+   if (remove("testwrite.txt" == 0)){
+
+    
+
+   }
+
+
+    return 0;
+}
