07c92c8a237593a01783e93bc2da23e243103337
jordanlee02 <jordan.lee02@outlook.com>
2024-04-23 23:22:18 +0100

    Completed writing files example

--- src/WritingFiles.c
+++ src/WritingFiles.c
@@ -3,18 +3,24 @@
 int main(){
 
 
-    /*
-    FILE *pF = fopen("testwrite.txt", "a");
+    
+    FILE *pF = fopen("testwrite.txt", "w");
 
     fprintf(pF, "\nSpongebob Sqaurepants");
 
     fclose(pF);
-    */
-   if (remove("testwrite.txt" == 0)){
+    
+
+   
+//     if(remove("testwrite.txt") == 0){
+//         printf("That file was deleted successfully");
+//     }
+//     else{
+//     printf("That file was NOT deleted!");
 
     
 
-   }
+//    }
 
 
     return 0;
